ca26808529ece3b64b35a7ea35f6b991ccbbf119
Daniel Whitney <whitneyd@southwestern.edu>
2020-10-03 13:20:05 -0500

    initial commit

---
+++ practice.js
@@ -0,0 +1,7 @@
+
+
+const twoSums = (nums, taget) =>{
+// given an array of integers nums and an integer target, return the indeces of the two numbers that add up to the target
+// you may assume that each input would ahve exactly one solution and may not use the same element twice
+// the answer can be returned in any order
+}

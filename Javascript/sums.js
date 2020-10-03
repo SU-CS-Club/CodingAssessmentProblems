9ea1a7e84fe38b3f05d4b61f0a55e9ea9326fae5
Daniel Whitney <whitneyd@southwestern.edu>
2020-10-03 15:11:46 -0500

    added threes sums with commenting

--- Javascript/sums.js
+++ Javascript/sums.js
@@ -5,7 +5,7 @@
 const twoSums = (nums, target) =>{
     // error check first
     if(nums.length < 2){
-        throw "Nums array must have 3 elements"
+        throw "Nums array must have 3 elements";
     }
     let forReturn;
     nums.forEach((val, ind)=>{
@@ -17,7 +17,13 @@ const twoSums = (nums, target) =>{
     })
     return forReturn;
 }
-
+/**
+ * given an array of integers nums and an integer target, return the indeces of the two numbers that add up to the target
+ * you may assume that each input would ahve exactly one solution and may not use the same element twice
+ * the answer can be returned in any order
+ * @param {int[]} nums 
+ * @param {int} target 
+ */
 const twoSumsWithHash = (nums, target)=>{
     let hashNums = {}; // instantiate hash then generate key/value pairs (O(n))
     nums.forEach((val, ind)=>{ // this is a speed/memory trade off - because we're taking advantage of hashing access which is O(1) we get faster lookup at the 
@@ -33,6 +39,42 @@ const twoSumsWithHash = (nums, target)=>{
         }
     }
 }
+//given an array of n integers nums and a target, find the number of index triplets (i, j, k) with 0 <= i < j < k < n that satisfy the condition nums[i] + nums[j] + nums[k] < target
+// example input nums = [-2, 0, 1, 3], target = 2
+// output should be 2
+// there are two triplets that are less than 2; [-2, 0, 1] and [-2, 0, 3]
+const threeSumSmaller = (nums, target)=>{
+    // first, lets check for cases that can't be computed
+    if(nums.length < 3) return 0 // can't find triplets with out at least three values
+    nums.sort((a, b)=> a - b) // js specific sort function - results in sorting the array smallest to largest (O(n))
+    let result = 0;;
+    for(let i = 0; i < nums.length; i++){ // start outer loop and initial start and end variables
+        let start = i + 1;
+        let end = nums.length -1;
+        while(start < end){ // now lets check the interior components of array
+            const sum = nums[i] + nums[start] + nums[end]; // we now have the sum of three of the internal indices
+            if(sum < target){ // affirmative case, increase result by the end value - start value
+                result += end - start; // because we're not returning distinct combinations we use end - start instead of incrementing (and doing additional checks)
+                console.log(" at index: " + i + " the result is: " + result + " which was made by end: " + end + " minus the start: " + start) 
+            }
+            if(sum < target){
+                // if affirmative then increase start value to check next index of start
+                start+=1;
+                
+            } else { // or decrement end value
+                end-=1;
+            }
+        }
+
+    }
+    return result;
+}
+
+
+
+
 
 console.log(twoSums([2, 7, 11, 15], 9))
 console.log(twoSumsWithHash([2, 7, 11, 15], 9))
+console.log(threeSumSmaller([-2, 0, 1, 3], 2))
+console.log(threeSumSmaller([2, 0, 0, 2, -2], 2))

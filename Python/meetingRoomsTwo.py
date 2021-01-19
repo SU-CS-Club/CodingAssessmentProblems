3022735d10a42a89c8ecd5beafa1aed3c04ec38f
DevonFulcher <devonfulcher3@gmail.com>
2021-01-19 08:23:42 -0600

    renamed some variables

--- Python/meetingRoomsTwo.py
+++ Python/meetingRoomsTwo.py
@@ -10,12 +10,12 @@ def minMeetingRooms(intervals: list[list[int]]) -> int:
     answer = 1
     sorted_intervals = sorted(intervals, key=lambda x: x[0])
     # add the first interval's end time to the heap
-    heap = [sorted_intervals[0][1]]
+    end_times_min_heap = [sorted_intervals[0][1]]
     for interval in sorted_intervals[1:]:
-        earliest_end_time = heapq.nsmallest(1, heap)[0]
-        if interval[0] < earliest_end_time:
-            heapq.heappush(heap, interval[1])
+        min_end_time = heapq.nsmallest(1, end_times_min_heap)[0]
+        if interval[0] < min_end_time:
+            heapq.heappush(end_times_min_heap, interval[1])
             answer += 1
         else:
-            heapq.heapreplace(heap, interval[1])
+            heapq.heapreplace(end_times_min_heap, interval[1])
     return answer
